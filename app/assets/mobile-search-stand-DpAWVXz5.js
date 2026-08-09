6b5d04615f648a2ecbfdd0afb6b498ebaf0ff689
nikitakozyr51-wq <nikitakozyr51@gmail.com>
2026-08-09 04:49:38 +0300

    демонстрация: сборка 2026-08-09 01:49

---
+++ app/assets/mobile-search-stand-DpAWVXz5.js
@@ -0,0 +1,1 @@
+import{n as e}from"./rolldown-runtime-B-9mzPXz.js";import{r as t}from"./react-vendor-CpDPhtGc.js";import{MobileSearchScreenPage as n,t as r}from"./mobile-search-screen-oahxSNTm.js";function i(){return(0,a.jsx)(n,{dataset:`measured`})}var a;e((()=>{r(),a=t()}))();export{i as MobileSearchStandPage};
